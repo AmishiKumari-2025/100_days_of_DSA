1557c83a73e4fa2e981f394af55cdb03446d3687
AmishiKumari-2025 <AMISHI.21610@stu.upes.ac.in>
2026-04-24 16:29:42 +0530

    Add files via upload

---
+++ Day83.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j, min_index, temp;
+
+    // Input size
+    scanf("%d", &n);
+
+    int arr[n];
+
+    // Input array elements
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Selection Sort
+    for(i = 0; i < n - 1; i++) {
+        min_index = i;
+
+        for(j = i + 1; j < n; j++) {
+            if(arr[j] < arr[min_index]) {
+                min_index = j;
+            }
+        }
+
+        // Swap
+        temp = arr[i];
+        arr[i] = arr[min_index];
+        arr[min_index] = temp;
+    }
+
+    // Print sorted array
+    for(i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
